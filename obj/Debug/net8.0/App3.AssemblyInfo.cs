87c5657d45a6c9c7557bd2ea19d995b23345da8a
Vijay Vishwakarma <vijay2388@gmail.com>
2025-01-18 07:48:05 +0530

    Index File Modified

--- obj/Debug/net8.0/App3.AssemblyInfo.cs
+++ obj/Debug/net8.0/App3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1994771dbc87e1be8cb56ed871021f092c484539")]
 [assembly: System.Reflection.AssemblyProductAttribute("App3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
